43fc79117341ee3ffa968b3f614fce584bff62fc
jliusl <liangliangyyy@163.com>
2025-08-09 15:23:12 +0800

    update decorator.py

--- python/src/decorator.py
+++ python/src/decorator.py
@@ -1,18 +1,23 @@
 import time
 from functools import wraps
 
-def timeing(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        elapsed = end - start
-        print(f'Elapsed time: {elapsed:.2f}s')
-        return result
-    return wrapper
+def timeing(unit: str):
 
-@timeing
+    def decorator(func):
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            start = time.time()
+            result = func(*args, **kwargs)
+            end = time.time()
+            elapsed = end - start
+            print(f'Elapsed time: {elapsed:.2f}{unit}')
+            return result
+        return wrapper
+    return decorator
+
+
+@timeing("秒")
 def test(n: int) -> None:
     time.sleep(n)
 
